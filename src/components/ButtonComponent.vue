25ea2bd03f1972002181d538b6edb7bc83a35f80
Jules Dupont <jules.dupont0709@gmail.com>
2025-02-13 11:41:25 +0100

    composant boutton reutilisable

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <button 
+    :class="data.class" 
+    :type="data.type">
+    </button>
+</template>
+
+<script setup lang="ts">
+
+interface ButtonComponentProperties{
+    data;{
+        class: string,
+        type: string,
+    }
+    
+}
+
+defineProps<ButtonComponentProperties>()
+
+</script>
